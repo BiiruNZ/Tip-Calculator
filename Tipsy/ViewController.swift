52aff7ab37c944a0c6cff609320ecfec9e3df3a1
Joe Zhang <Biiru@Joes-MacBook.local>
2017-01-04 17:25:18 -0800

    Here is my commit message

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.billField .becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
 
-}
+   
+    @IBAction func CalcTip(_ sender: AnyObject) {
+        
+        let tipPercentage = [0, 0.15, 0.18, 0.20]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+   }
 
